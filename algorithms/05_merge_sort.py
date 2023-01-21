2c878a754f8aa1f67ae2b3c2ab8194a9f1868645
mark0g111 <9154547225@mail.ru>
2023-01-21 10:30:14 +0300

    merge sort

---
+++ algorithms/05_merge_sort.py
@@ -0,0 +1,32 @@
+from algorithms.random_array import random_array
+
+
+def merge_sort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        # i - left array index; j - right array index; k - main array index
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+    return array
+
+
+print(merge_sort(random_array(10)))
